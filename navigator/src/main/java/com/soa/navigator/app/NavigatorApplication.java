eaa1807c825380127d0335facce86495f44ec6d1
myav <Myavochka119@gmail.com>
2024-10-04 01:45:56 +0300

    Expanded templates for navigator methods

--- navigator/src/main/java/com/soa/navigator/app/NavigatorApplication.java
+++ navigator/src/main/java/com/soa/navigator/app/NavigatorApplication.java
@@ -1,9 +1,11 @@
 package com.soa.navigator.app;
 
+import com.soa.navigator.model.Location;
 import com.soa.navigator.model.Route;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.client.WebTarget;
 import jakarta.ws.rs.core.Application;
 import jakarta.ws.rs.core.GenericType;
@@ -30,16 +32,36 @@ public class NavigatorApplication extends Application {
                                                  @PathParam("id-to") Long idTo,
                                                  @PathParam("order-by") String orderBy) {
         List<Route> routes = target.path("/").queryParam("filter", "idFrom=" + idFrom + "&idTo=" + idTo)
-                .request(MediaType.APPLICATION_JSON).get(new GenericType<>() {});
+                .request(MediaType.APPLICATION_JSON).get(new GenericType<>() {
+                });
         return okWith(routes);
     }
 
 
     @POST
     @Path("/route/add/{id-from}/{id-to}/{distance}")
-    public Response addRouteBetweenLocations(@PathParam("id-from") Long idFrom, @PathParam("id-to") Long idTo, @PathParam("distance") Double distance) {
-        return null;
-        //return okWith();
+    public Response addRouteBetweenLocations(@PathParam("id-from") Long idFrom, @PathParam("id-to") Long idTo, @PathParam("distance") Integer distance) {
+        Route route = new Route();
+        //TODO create correct Route
+        Location from = new Location();
+        from.setName("from");
+        from.setX(1.0);
+        from.setY(1);
+        from.setZ(1F);
+
+
+        Location to = new Location();
+        to.setName("to");
+        to.setX(2.0);
+        to.setY(2);
+        to.setZ(2F);
+
+        route.setFrom(from);
+        route.setTo(to);
+        route.setDistance(distance);
+        return target.path("/route").request(MediaType.APPLICATION_JSON)
+                .post(Entity.entity(route, MediaType.APPLICATION_JSON));
     }
+
 
 }
