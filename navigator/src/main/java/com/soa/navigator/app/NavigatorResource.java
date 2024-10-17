8e6b53de615ce75cfac85a7d5d433f62a6fc99cb
myav <Myavochka119@gmail.com>
2024-10-17 19:59:53 +0300

    Made possible to deploy it on payara

---
+++ navigator/src/main/java/com/soa/navigator/app/NavigatorResource.java
@@ -0,0 +1,132 @@
+package com.soa.navigator.app;
+
+import com.soa.navigator.helpers.validation.Order;
+import com.soa.navigator.model.Location;
+import com.soa.navigator.model.Route;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.Entity;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.GenericType;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.List;
+
+@Path("/navigator")
+public class NavigatorResource {
+    private final String coreServerUrl = "localhost:8080/routes";
+    private final Client client = ClientBuilder.newClient();
+    private final WebTarget target = client.target(coreServerUrl);
+
+    private Response okWith(Object entity) {
+        return Response.ok(entity).build();
+    }
+
+    boolean isResponseStatusOK(Response response) {
+        return response.getStatus() == Response.Status.OK.getStatusCode();
+    }
+
+    boolean isResponseStatusCrashed(Response response) {
+        return response.getStatus() == Response.Status.INTERNAL_SERVER_ERROR.getStatusCode();
+    }
+
+    @GET
+    @Path("/hello")
+    @Produces("application/json")
+    public Response hello() {
+        String hello = "Hello";
+        return Response.ok(hello).build();
+    }
+
+
+    @GET
+    @Path("/routes/{id-from}/{id-to}/{order-by}")
+    @Produces("application/json")
+    public Response getAllRoutesBetweenLocations(@PathParam("id-from") Long idFrom, @PathParam("id-to") Long idTo, @PathParam("order-by") String orderBy) {
+        Response routesResponse = target.path("/").queryParam("filter", "filter=[from.id=" + idFrom + ",to.id=" + idTo + "]").request(MediaType.APPLICATION_JSON).get();
+
+        if (isResponseStatusOK(routesResponse)) {
+            List<Route> routes = routesResponse.readEntity(new GenericType<List<Route>>() {
+            });
+            if (routes.isEmpty()) return Response.status(Response.Status.NOT_FOUND).build();
+            order(routes, orderBy);
+            return okWith(routes);
+        } else {
+            System.out.println("request status is: " + routesResponse.getStatus());
+            if (isResponseStatusCrashed(routesResponse))
+                return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+            else return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+    }
+
+
+    @POST
+    @Path("/route/add/{id-from}/{id-to}/{distance}")
+    public Response addRouteBetweenLocations(@PathParam("id-from") Long idFrom, @PathParam("id-to") Long idTo, @PathParam("distance") Integer distance) {
+        Response routesWithFrom = target.path("/").queryParam("filter", "idFrom=" + idFrom).request(MediaType.APPLICATION_JSON).get(new GenericType<>() {
+        });
+        Response routesWithTo = target.path("/").queryParam("filter", "idFrom=" + idFrom).request(MediaType.APPLICATION_JSON).get(new GenericType<>() {
+        });
+        if (isResponseStatusOK(routesWithFrom) && isResponseStatusOK(routesWithTo)) {
+            List<Route> routesFrom = routesWithFrom.readEntity(new GenericType<List<Route>>() {
+            });
+            List<Route> routesTo = routesWithFrom.readEntity(new GenericType<List<Route>>() {
+            });
+            if (!routesFrom.isEmpty() && !routesTo.isEmpty()) {
+                Location locationFrom = routesFrom.get(0).getFrom();
+                Location locationTo = routesTo.get(0).getTo();
+                Route route = new Route();
+
+                route.setName("from_" + locationFrom.getName() + "_to_" + locationTo.getName() + "_distance_" + distance);
+                route.setFrom(locationFrom);
+                route.setTo(locationTo);
+                route.setDistance(distance);
+
+                return target.path("/route").request(MediaType.APPLICATION_JSON).post(Entity.entity(route, MediaType.APPLICATION_JSON));
+
+            } else return Response.status(Response.Status.BAD_REQUEST).build();
+        } else if (isResponseStatusCrashed(routesWithFrom) || isResponseStatusCrashed(routesWithTo)) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        } else return Response.status(Response.Status.BAD_REQUEST).build();
+
+    }
+
+
+    void order(List<Route> routes, String orderBy) {
+        routes.sort((route1, route2) -> {
+            try {
+                Order orderField = Order.valueOf(orderBy.toUpperCase());
+                System.out.println(orderField + " " + orderBy);
+                switch (orderField) {
+                    case NAME:
+                        return route1.getName().compareTo(route2.getName());
+                    case FROM_X:
+                        return Double.compare(route1.getFrom().getX(), route2.getFrom().getX());
+                    case FROM_Y:
+                        return Long.compare(route1.getFrom().getY(), route2.getFrom().getY());
+                    case FROM_NAME:
+                        return route1.getFrom().getName().compareTo(route2.getFrom().getName());
+                    case TO_X:
+                        return Double.compare(route1.getTo().getX(), route2.getTo().getX());
+                    case TO_Y:
+                        return Long.compare(route1.getTo().getY(), route2.getTo().getY());
+                    case TO_NAME:
+                        return route1.getTo().getName().compareTo(route2.getTo().getName());
+                    case COORDINATES_X:
+                        return Double.compare(route1.getCoordinates().getX(), route2.getCoordinates().getX());
+                    case COORDINATES_Y:
+                        return Long.compare(route1.getCoordinates().getY(), route2.getCoordinates().getY());
+                    case DISTANCE:
+                        return Double.compare(route1.getDistance(), route2.getDistance());
+                    default:
+                        return 0;
+                }
+            } catch (IllegalArgumentException e) {
+                System.err.println("Неверный параметр сортировки: " + orderBy);
+                return 0;
+            }
+        });
+    }
+}
