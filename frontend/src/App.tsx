4439f01eed561960dfb70ba23a577e8651c97eb8
myav <Myavochka119@gmail.com>
2024-10-18 22:34:38 +0300

    Changed route table structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -144,16 +144,48 @@ function App() {
                 <table>
                     <thead>
                     <tr>
-                        <th>Номер</th>
-                        <th>Маршрут</th>
-                        <th>Действия</th>
+                        <th rowSpan={2}>Номер</th>
+                        <th rowSpan={2}>id</th>
+                        <th rowSpan={2}>Название</th>
+                        <th colSpan={2}>Координаты</th>
+                        <th rowSpan={2}>Дата создания</th>
+                        <th colSpan={4}>Откуда</th>
+                        <th colSpan={4}>Куда</th>
+                        <th rowSpan={2}>Расстояние</th>
+                        <th rowSpan={2}>Действия</th>
+                    </tr>
+                    <tr>
+                        <th>x</th>
+                        <th>y</th>
+                        <th>Имя</th>
+                        <th>x</th>
+                        <th>y</th>
+                        <th>z</th>
+                        <th>Имя</th>
+                        <th>x</th>
+                        <th>y</th>
+                        <th>z</th>
                     </tr>
                     </thead>
                     <tbody>
                     {routes.map((route, index) => (
                         <tr key={index}>
                             <td>{index + 1}</td>
-                            <td>{JSON.stringify(route)}</td>
+                            <td>{route.id}</td>
+                            <td>{route.name}</td>
+                            <td>{route.coordinates.x}</td>
+                            <td>{route.coordinates.y}</td>
+                            <td>{route.creationDate}</td>
+                            <td>{route.from.name}</td>
+                            <td>{route.from.x}</td>
+                            <td>{route.from.y}</td>
+                            <td>{route.from.z}</td>
+                            <td>{route.to.name}</td>
+                            <td>{route.to.x}</td>
+                            <td>{route.to.y}</td>
+                            <td>{route.to.z}</td>
+                            <td>{route.distance}</td>
+
                             <td>
                                 <button onClick={() => handleEditRoute(route)}>Подробнее</button>
                                 <button>Удалить</button>
@@ -162,6 +194,8 @@ function App() {
                     ))}
                     </tbody>
                 </table>
+
+
                 <div style={{textAlign: 'center', marginTop: '50px'}} className="menu">
                     <div>
                         {Object.keys(actions).map((key) => (
