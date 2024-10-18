6b4c2c375348fa0fc6256efd5486ad7d775e6f83
kshprenger <kostya@shprenger.com>
2024-10-19 01:09:48 +0300

    squash! Cors patch

--- core/src/main/java/core/rest/CorsFilter.java
+++ core/src/main/java/core/rest/CorsFilter.java
@@ -22,6 +22,6 @@ public class CorsFilter implements ContainerResponseFilter {
                 "origin, content-type, accept, authorization");
         responseContext.getHeaders().add(
                 "Access-Control-Allow-Methods",
-                "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+                "GET, POST, PUT, DELETE, PATCH, OPTIONS, HEAD");
     }
 }
