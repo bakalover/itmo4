6e66a414b4daae16d5ed7151c06d9dac4dc1eb58
KristinaKravtsova <Myavochka119@gmail.com>
2024-11-29 22:11:11 +0300

    Fix Zuul-Eureka-Navigator interaction

--- navigator-spring/zuul/zuul/src/main/java/com/soa/zuul/ZuulApplication.java
+++ navigator-spring/zuul/zuul/src/main/java/com/soa/zuul/ZuulApplication.java
@@ -2,41 +2,14 @@ package com.soa.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-import com.soa.zuul.filters.PreFilter;
-import com.soa.zuul.filters.PostFilter;
-import com.soa.zuul.filters.ErrorFilter;
-import com.soa.zuul.filters.RouteFilter;
 
 @SpringBootApplication
 @EnableZuulProxy
-@EnableEurekaClient
-
+@EnableDiscoveryClient
 public class ZuulApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ZuulApplication.class, args);
-    }
-
-    @Bean
-    public PreFilter preFilter() {
-        return new PreFilter();
-    }
-
-    @Bean
-    public PostFilter postFilter() {
-        return new PostFilter();
-    }
-
-    @Bean
-    public ErrorFilter errorFilter() {
-        return new ErrorFilter();
-    }
-
-    @Bean
-    public RouteFilter routeFilter() {
-        return new RouteFilter();
     }
 }
